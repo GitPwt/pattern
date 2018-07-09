97993072b2cc1bd62cd992287533c36f8baf8e81
Parag <parag@pritams-macbook-pro.local>
2018-07-09 11:54:54 +0530

    Initial Commit

--- TestRun/ViewController.swift
+++ TestRun/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblDisplay: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        printPattern(option: 4)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +25,68 @@ class ViewController: UIViewController {
     }
 
 
+    func printPattern( option : Int ) -> Void {
+        
+        switch option{
+        case 1:// Simple
+            for _ in 1...5{
+                
+                for _ in 1...5{
+                    
+                    print("*", terminator: " ")
+                }
+                
+                print("\n", terminator: "")
+            }
+            
+        case 2:// Triangle (Right angled)
+            for i in 1...5{
+                
+                for _ in 1...i{
+                    
+                    print("*", terminator: " ")
+                }
+                
+                print("\n", terminator: "")
+            }
+            
+        case 3:
+            
+            for i in 1...5{
+                
+                for _ in (i...5).reversed(){
+                    
+                    print(" ", terminator: " ")
+                }
+                
+                for _ in (1...i){
+
+                    print("*", terminator: " ")
+                }
+                
+                print("\n", terminator: "")
+            }
+        case 4:
+            
+            for i in 1...5{
+                
+                for _ in 1...i{
+                    
+                    print(" ", terminator: " ")
+                }
+                
+                for _ in (i...5).reversed(){
+                    
+                    print("*", separator: "", terminator: " ")
+                }
+                
+                print("\n", terminator: "")
+            }
+        default:
+            break
+        }
+        
+        
+    }
 }
 
